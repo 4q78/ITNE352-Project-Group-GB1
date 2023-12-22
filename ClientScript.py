14636d123c390e4bf19c8098e697052c6aa31604
Abbas Abdulaziz <qabbasp88@gmail.com>
2023-12-22 23:51:27 +0300

    Initial code

--- ClientScript.py
+++ ClientScript.py
@@ -1,1 +1,22 @@
-print("Hello")
+import socket
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(("127.0.0.1",6666))
+name=input("Enter your name:")
+client.send(name.encode('utf-8'))
+option=input("press Enter to start\n")
+while option!=0:
+       print("1. print Arrived flights")
+       print("2. Gender and Experience")
+       print("3. Position Title and Position # ")
+       print("4. employee names and their Position #")
+       print("5. Quit")
+       option=input("Enter the Flight option(not number)\n")
+       client.sendto(option.encode('utf-8'),("127.0.0.1",9999)) 
+       if option.casefold=="arrived flights":
+         print(client.recv(2048).decode('utf-8'))
+         
+
+       if option=='5':
+           client.sendto("quit".encode('utf-8'),("127.0.0.1",9999))
+           print(client.recvfrom(1024)[0].decode('utf-8'))
+           break
--- ServerScript.py
+++ ServerScript.py
@@ -1,1 +1,33 @@
-print("Hello")
+import requests
+import json
+import socket
+import threading
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(("127.0.0.1",6666))
+server.listen(3)
+def Connection():
+    code=input("Enter the airport code\n")
+    def apiParameters(parameters):
+       api=requests.get('https://api.aviationstack.com/v1/flights',parameters)
+       with open("GB1_202105138/202106345.jason","w") as k:
+        json.dumps(api.json(),indent=4)
+    message=server.recv(1024).decode('utf-8')
+    print(message)
+    st1=server.recv(1024).decode
+    jsonFile="GB1_202105138/202106345.jason"
+    st1=(str)
+    while st1.casefold() !="quit":
+       if(st1=="arrived flights".casefold()):
+           parameters={
+          'access_key': '1d815a01512667373760dd9a41070d0f',
+          'arr_icao':code,
+          'flight_status':'landed'
+}
+           with open(jsonFile,'r') as k:
+              information=clientSocket.sendall(json.load(k))  
+clientSocket,address=server.accept()
+t1=threading.Thread(target=Connection)
+t1.start()
+
+
+
