74533ec82f9ed1df34e5ef01fd162028c7f2ac9b
Abbas Abdulaziz <qabbasp88@gmail.com>
2023-12-23 20:47:03 +0300

    missed to delete the unwanted code so am pushing again

--- ServerScript.py
+++ ServerScript.py
@@ -2,38 +2,6 @@ import requests
 import json
 import socket
 import threading
-'''server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(("127.0.0.1",6666))
-server.listen(3)
-def Connection(sock,id):
-    code=input("Enter the airport code\n")
-    def apiParameters(parameters):
-       api=requests.get('http://api.aviationstack.com/v1/flights',parameters)
-       jsonValue=json.dumps(api.json(),indent=4)
-       with open("GB1_202105138/202106345.jason","w") as k:
-          k.write(jsonValue)
-    message=server.recv(1024).decode('utf-8')
-    print(message)
-    st1=server.recv(1024).decode
-    jsonFile="GB1_202105138/202106345.jason"
-    st1=(str)
-    while st1.casefold() !="quit":
-       if(st1=="arrived flights".casefold()):
-           parameters={
-          'access_key': '1d815a01512667373760dd9a41070d0f',
-          'arr_icao':code,
-          'flight_status':'landed'
-}
-           with open(jsonFile,'r') as k:
-              information=clientSocket.sendall(json.load(k)) 
-              
-numThreads=[] 
-clientSocket,address=server.accept()
-t1=threading.Thread(target=Connection,args=(clientSocket,len(numThreads)+1))
-numThreads.append(t1)
-t1.start()
-clientSocket.close()'''
-
 server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(("127.0.0.1",6666))
 server.listen(3)
