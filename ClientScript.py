b5ed8662d6aa2d07176a5b7397bcbbff488a09ca
Abbas Abdulaziz <qabbasp88@gmail.com>
2023-12-26 20:11:37 +0300

    Finisehed all the work did the additional part (secure login system) and added the textfile

--- ClientScript.py
+++ ClientScript.py
@@ -1,7 +1,31 @@
 import socket
-client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(("127.0.0.1",6666))
-name=input("Enter your name:")
+import time
+from inputimeout import inputimeout 
+x=True
+exceptTest=False
+while x:
+   try:
+    client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client.connect(("127.0.0.1",6666))
+    if client.getpeername():
+      print("Coonected")
+      break
+   except:
+     print("Failed to connect to server will try again in 8 seconds")
+     time.sleep(8)
+     exceptTest=True
+   if not exceptTest:
+     x=False
+while True:
+  message=client.recv(1024).decode('utf-8')
+  client.send(input(message).encode())
+  message=client.recv(1024).decode('utf-8')
+  client.send(input(message).encode())
+  message=client.recv(1024).decode('utf-8')
+  print(message)
+  if message.casefold()=="login successful!":
+    break
+name=inputimeout(prompt="Enter your name:", timeout=60)
 client.send(name.encode('utf-8'))
 option=input("press Enter to start\n")
 while option!='0':
@@ -10,14 +34,13 @@ while option!='0':
   print("3. print All flights from a specific airport using the airport ICAO code  ")
   print("4. print Details of a particular flight ")
   print("5. Quit")
-  option=input("Enter option number:\n")
+  option=inputimeout(prompt="Enter option number:\n",timeout=60)
   client.send(option.encode('utf-8')) 
   if option=='1':
     print(client.recv(2048).decode('utf-8'))
     print(client.recv(2048).decode('utf-8'))
     print(client.recv(2048).decode('utf-8'))
     print(client.recv(2048).decode('utf-8'))
-    print("Am here")
   if option=='2':
     print(client.recv(2048).decode('utf-8'))
     print(client.recv(2048).decode('utf-8'))
@@ -28,12 +51,12 @@ while option!='0':
     print(client.recv(2048).decode('utf-8'))
     print(client.recv(2048).decode('utf-8'))
   if option=='3':
-    message=input("Enter airport ICAO code:")
+    message=input("Enter deparure airport:")
     client.send(message.encode('utf-8'))
     print(client.recv(2048).decode('utf-8'))
     print(client.recv(2048).decode('utf-8'))
   if option=='4':
-    message=input("Enter the flight number:")
+    message=input("Enter the flight iata:")
     client.send(message.encode('utf-8'))
     print(client.recv(2048).decode('utf-8'))
     print(client.recv(2048).decode('utf-8'))
--- ServerScript.py
+++ ServerScript.py
@@ -3,9 +3,12 @@ import json
 import socket
 import threading
 import time
+import sqlite3
+import hashlib
 server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(("127.0.0.1",6666))
 server.listen(3)
+print("Wating for connection....")
 timeoutSec=60
 '''code=input("Enter the airport code\n")
 params = {
@@ -23,16 +26,19 @@ apiParameters(params=params)'''
 def Clients(ClientSocket,address):
     print(f'connected to {address}')
     ClientSocket.settimeout(timeoutSec)
+    check_client_login(ClientSocket)
     try:
        Namemessage=ClientSocket.recv(1024).decode('utf-8')
        print(Namemessage)
        message=ClientSocket.recv(1024).decode('utf-8')
        if not message or not Namemessage:
-          return
+          raise socket.timeout
        x=True
        while x:
          if message=='1':  
-            print(f'name:{Namemessage} |type of request: All arrived flights (return flight IATA code, departure airport name,\n arrival time, arrival terminal number, and arrival gate).')      
+            print(f'name:{Namemessage} |type of request: All arrived flights (return flight IATA code, departure airport name,\n arrival time, arrival terminal number, and arrival gate).')    
+            print(80*"-")
+            print()  
             with open('GB1.json','r') as k:
                reader=json.load(k)
                iata=[]
@@ -60,6 +66,8 @@ def Clients(ClientSocket,address):
             ClientSocket.send(json.dumps(Dictgate,indent=4).encode('utf-8'))
          if message=='2':
             print(f'name:{Namemessage} |type of request: All delayed flights (return flight IATA code, departure airport,\n original departure time, the estimated time of arrival), arrival terminal, delay, and arrival gate.')
+            print(80*"-")
+            print()
             with open('GB1.json','r') as k:
                reader=json.load(k)
                iata=[]
@@ -71,7 +79,7 @@ def Clients(ClientSocket,address):
                gate=[]
 
                for search in reader['data']:
-                  if search['departure']['delay']!='null' or search['arrival']['delay']!='null':
+                  if search['arrival']['delay']!='null':
                      iata.append(search['flight']['iata'])
                      airport.append(search['departure']['airport'])
                      Dactual.append(search['departure']['actual'])
@@ -95,6 +103,8 @@ def Clients(ClientSocket,address):
             ClientSocket.send(json.dumps(Dictgate,indent=4).encode('utf-8'))
          if message=='3':
             print(f'name:{Namemessage} | type of request: All flights from a specific airport using the airport ICAO code (return flight IATA code, \n departure airport, original departure time, estimated arrival time, departure gate, arrival gate, and status).')
+            print(80*"-")
+            print()
             message=ClientSocket.recv(1024).decode('utf-8')
             with open('GB1.json','r') as k:
                reader=json.load(k)
@@ -108,7 +118,7 @@ def Clients(ClientSocket,address):
                
 
                for search in reader['data']:
-                  if search['departure']['icao']==message:
+                  if search['departure']['airport']==message:
                      iata.append(search['flight']['iata'])
                      airport.append(search['departure']['airport'])
                      Dactual.append(search['departure']['actual'])
@@ -134,6 +144,8 @@ def Clients(ClientSocket,address):
             ClientSocket.send(json.dumps(Dictstatus,indent=4).encode('utf-8'))
          if message=='4':
             print(f'name:{Namemessage} |type of request: Details of a particular flight (return flight IATA code; \n departure airport,gate, and terminal; arrival airport, gate, and terminal; \n status; scheduled departure time; and scheduled arrival time).')
+            print(80*"-")
+            print()
             message=ClientSocket.recv(1024).decode('utf-8')
             with open('GB1.json','r') as k:
                reader=json.load(k)
@@ -150,7 +162,7 @@ def Clients(ClientSocket,address):
                
 
                for search in reader['data']:
-                  if search['flight']['number']==message:
+                  if search['flight']['iata']==message:
                      iata.append(search['flight']['iata'])
                      airport.append(search['departure']['airport'])
                      DepGate.append(search['departure']['gate'])
@@ -194,10 +206,29 @@ def Clients(ClientSocket,address):
         ClientSocket.close()
     except socket.timeout:
             print(f"Client {address} timed out. Closing the connection.")
-         
+            print() 
+            return  
+    finally:
+       ClientSocket.close()
+def check_client_login(ClientSocket):
+   while True:
+    ClientSocket.send("username: ".encode('utf-8'))
+    username=ClientSocket.recv(1024).decode('utf-8')
+    ClientSocket.send("password: ".encode('utf-8'))
+    password=ClientSocket.recv(1024)
+    password=hashlib.sha256(password).hexdigest()
+    Dbconnect=sqlite3.connect("userdata.db")
+    cur=Dbconnect.cursor()
+    cur.execute("SELECT * FROM userdata WHERE username =? AND password=?",(username,password))
+    if cur.fetchall():
+       ClientSocket.send("login successful!".encode('utf-8'))
+       break
+    else:
+       ClientSocket.send("login failed!".encode('utf-8'))
+
 while True:
     ClientSocket,address=server.accept()
-    threading.Thread(target=Clients,args=(ClientSocket,address)).start()         
+    threading.Thread(target=Clients,args=(ClientSocket,address)).start()        
             
 
          
